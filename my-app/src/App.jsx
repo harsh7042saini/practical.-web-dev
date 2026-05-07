70dc36d33acc8e6a99be72b85056482758f34991
harsh7042saini <harsh7042saini@gmail.com>
2026-05-07 11:13:13 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Child from "./Child";
 
 function App() {
-  const name = "Harsh saini";
-  const age = 19;
+
+  // Props Data
+  const name = "Harsh";
+  const age = 21;
+
+  // API Data
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
   return (
     <div>
-      <h1>Parent Component</h1>
+
+      {/* Props Question */}
+      <h1>Props Example</h1>
+
       <Child name={name} age={age} />
+
+      <hr />
+
+      {/* Fetch API Question */}
+      <h1>Fetch API Example</h1>
+
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <hr />
+        </div>
+      ))}
+
     </div>
   );
 }
 
-export  default app;    
- 
+export default App;
